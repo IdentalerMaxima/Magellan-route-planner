6b076ef8500498d7daf9620520d1697534e7a9f6
rikerik <bako.erik.dominik@gmail.com>
2022-12-18 18:22:31 +0100

    js teszt, css alapok fixált navbarral és legördüő menűvel

--- Magellan/target/Magellan-1.0-SNAPSHOT/webApp/exp/script.js
+++ Magellan/target/Magellan-1.0-SNAPSHOT/webApp/exp/script.js
@@ -1,14 +1,15 @@
-function getApi(){
-var url = "http://127.0.0.1:8080/Magellan-1.0-SNAPSHOT/api/teszt_get";
+function getApi() {
+    var url = "http://127.0.0.1:8080/Magellan-1.0-SNAPSHOT/api/teszt_get";
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        document.write(xhr.status);
-        document.write(xhr.responseText);
-    }};
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            document.write("státuszkód: ", xhr.status);
+            document.write("\tGET kérés eredménye: ", xhr.responseText);
+        }
+    };
 
-xhr.send();
+    xhr.send();
 }
