d54ba7c13a39bc01e2f526160f9073847ae24e76
rikerik <rikerik7@gmail.com>
2022-12-31 02:14:38 +0100

    login page, index

--- Magellan/target/Magellan-1.0-SNAPSHOT/webApp/exp/script.js
+++ Magellan/target/Magellan-1.0-SNAPSHOT/webApp/exp/script.js
@@ -1,15 +1,5 @@
-function getApi() {
-    var url = "http://127.0.0.1:8080/Magellan-1.0-SNAPSHOT/api/route/test";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            document.write("státuszkód: ", xhr.status);
-            document.write("\tGET kérés eredménye: ", xhr.responseText);
-        }
-    };
-
-    xhr.send();
-}
+$("button").click(function(){
+    $.get("demo_test.asp", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+});
